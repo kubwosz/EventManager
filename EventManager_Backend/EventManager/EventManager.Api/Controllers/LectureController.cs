11783f9e55a0e9e91afc78a8cd5f2fbf51f563d2
unknown <marekkinga97@gmail.com>
2018-03-30 01:15:21 +0200

    013 - added HttpGet

---
+++ EventManager_Backend/EventManager/EventManager.Api/Controllers/LectureController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventManager.Domain.Dtos;
+using EventManager.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class LectureController : Controller
+    {
+        private readonly LectureService _lectureService;
+
+        public LectureController()
+        {
+            _lectureService = new LectureService();
+        }
+
+        [HttpPost]
+        [Route("CreateLecture")]
+        public IActionResult CreateLecture([FromBody] AddLectureDto addLectureDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(_lectureService.AddLecture(addLectureDto));
+        }
+
+        [HttpGet]
+        [Route("Lectures")]
+        public IActionResult GetAll()
+        {
+            return Ok(_lectureService.GetAll());
+        }
+    }
+}
---
+++ EventManager_Backend/EventManager/EventManager.Domain/Services/LectureService.cs
@@ -0,0 +1,72 @@
+﻿using EventManager.Domain.Dtos;
+using EventManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventManager.Domain.Services
+{
+    public class LectureService
+    {
+        private readonly EventManagerContext _context;
+
+        public LectureService()
+        {
+            _context = new EventManagerContext();
+        }
+
+        public LectureDto AddLecture(AddLectureDto addLectureDto)
+        {
+            if (!_context.Events.Any(x => x.Id == addLectureDto.EventId))
+                return null;
+
+            var lecture = new Lecture()
+            {
+                EventId = addLectureDto.EventId,
+                Description = addLectureDto.Description,
+                Name = addLectureDto.Name,
+                ParticipantNumber = addLectureDto.ParticipantNumber,
+                StartDate = addLectureDto.StartDate,
+                EndDate = addLectureDto.EndDate
+            };
+
+            _context.Lectures.Add(lecture);
+            _context.SaveChanges();
+
+            var lectureDto = new LectureDto()
+            {
+                Id = lecture.Id,
+                EventId = lecture.EventId,
+                Description = lecture.Description,
+                Name = lecture.Name,
+                ParticipantNumber = lecture.ParticipantNumber,
+                StartDate = lecture.StartDate,
+                EndDate = lecture.EndDate
+            };
+            return lectureDto;
+        }
+
+        public List<LectureDto> GetAll()
+        {
+            var lecturesList = _context.Lectures.Select(x => x);
+
+            List<LectureDto> lectureDtos = new List<LectureDto>();
+
+            foreach (var lecture in lecturesList)
+            {
+                lectureDtos.Add(new LectureDto()
+                {
+                    Id = lecture.Id,
+                    EventId = lecture.EventId,
+                    Description = lecture.Description,
+                    Name = lecture.Name,
+                    ParticipantNumber = lecture.ParticipantNumber,
+                    StartDate = lecture.StartDate,
+                    EndDate = lecture.EndDate
+                });
+            }
+            return lectureDtos;
+        }
+    }
+}
