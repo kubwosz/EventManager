cbf3e7c6c4bcbc9845ac5eb928b69cfab421f143
Jakub Woszczyna <kubwosz@gmail.com>
2018-03-30 00:39:58 +0200

    Working editing a review

---
+++ EventManager_Backend/EventManager/EventManager.Domain/Dtos/ReviewDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManager.Domain.Dtos
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+
+        public int LectureId { get; set; }
+
+        public int ReviewerId { get; set; }
+
+        public int Rate { get; set; }
+
+        public string Nickname { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
