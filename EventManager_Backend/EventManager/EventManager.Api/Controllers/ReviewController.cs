f54924bafcaa5287eff3f563e1f408714ded9517
Jakub Woszczyna <kubwosz@gmail.com>
2018-03-29 23:42:14 +0200

    Creating a review with controller

---
+++ EventManager_Backend/EventManager/EventManager.Api/Controllers/ReviewController.cs
@@ -0,0 +1,41 @@
+﻿using EventManager.Domain.Dtos;
+using EventManager.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReviewController : Controller
+    {
+        private readonly ReviewService _reviewService;
+
+        public ReviewController()
+        {
+            _reviewService = new ReviewService();
+        }
+
+        //[HttpGet]
+        //[Route("Reviews")]
+        [HttpGet, Route("Reviews")]
+        public IActionResult GetAllReviews()
+        {
+            return Ok(_reviewService.GetAll());
+        }
+
+        [HttpPost]
+        [Route("CreateReview")]
+        public IActionResult CreateReview([FromBody] CreateReviewDto createReviewDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_reviewService.CreateReview(createReviewDto));
+        }
+    }
+}
--- EventManager_Backend/EventManager/EventManager.Domain/Services/ReviewService.cs
+++ EventManager_Backend/EventManager/EventManager.Domain/Services/ReviewService.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace EventManager.Domain.Services
 {
-    class ReviewService
+    public class ReviewService
     {
         private readonly EventManagerContext _context;
 
@@ -18,7 +18,7 @@ namespace EventManager.Domain.Services
 
         public ReviewDto CreateReview(CreateReviewDto addReviewDto)
         {
-            if (!(_context.SimpleUsers.Any(x => x.Id == addReviewDto.ReviewerId) && _context.SimpleUsers.Any(x=> x.Id == addReviewDto.LectureId)))
+            if (!(_context.SimpleUsers.Any(x => x.Id == addReviewDto.ReviewerId) && _context.Lectures.Any(x=> x.Id == addReviewDto.LectureId)))
             {
                 return null;
             }
@@ -46,6 +46,33 @@ namespace EventManager.Domain.Services
             };
 
             return reviewDto;
+        }
+
+        public List<ReviewDto> GetAll()
+        {
+            var result = _context.Reviews.Select(x => x);
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            List<ReviewDto> reviewDtos = new List<ReviewDto>();
+
+            foreach (var item in result)
+            {
+                reviewDtos.Add(new ReviewDto()
+                {
+                    Id = item.Id,
+                    LectureId = item.LectureId,
+                    ReviewerId = item.ReviewerId,
+                    Rate = item.Rate,
+                    Nickname = item.Nickname,
+                    Comment = item.Comment
+                });
+            }
+
+            return reviewDtos;
         }
     }
 }
