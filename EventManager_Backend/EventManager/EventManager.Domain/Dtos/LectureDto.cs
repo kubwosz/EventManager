18e17507297926e5d6274dcb8837f34c6d2f55e0
unknown <marekkinga97@gmail.com>
2018-03-29 21:41:23 +0200

    Added HttpPost, Dtos and services for Lectures

--- EventManager_Backend/EventManager/EventManager.Domain/Dtos/LectureDto.cs
+++ EventManager_Backend/EventManager/EventManager.Domain/Dtos/LectureDto.cs
@@ -6,5 +6,28 @@ namespace EventManager.Domain.Dtos
 {
     public class LectureDto
     {
+        public LectureDto()
+        {
+            LectureUsers = new List<LectureUserDto>();
+            Reviews = new List<ReviewDto>();
+        }
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public int EventId { get; set; }
+
+        public int ParticipantNumber { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public List<LectureUserDto> LectureUsers { get; set; }
+
+        public List<ReviewDto> Reviews { get; set; }
     }
 }
---
+++ EventManager_Backend/EventManager/EventManager.Domain/Services/LectureService.cs
@@ -0,0 +1,51 @@
+﻿using EventManager.Domain.Dtos;
+using EventManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventManager.Domain.Services
+{
+    public class LectureService
+    {
+        private readonly EventManagerContext _context;
+
+        public LectureService()
+        {
+            _context = new EventManagerContext();
+        }
+
+        public LectureDto AddLecture(AddLectureDto addLectureDto)
+        {
+            if (!_context.Events.Any(x => x.Id == addLectureDto.EventId))
+                return null;
+
+            var lectureToDb = new Lecture()
+            {
+                EventId = addLectureDto.EventId,
+                Description = addLectureDto.Description,
+                Name = addLectureDto.Name,
+                ParticipantNumber = addLectureDto.ParticipantNumber,
+                StartDate = addLectureDto.StartDate,
+                EndDate = addLectureDto.EndDate
+            };
+
+            _context.Lectures.Add(lectureToDb);
+            _context.SaveChanges();
+
+            var lectureDto = new LectureDto()
+            {
+                Id = lectureToDb.Id,
+                EventId = addLectureDto.EventId,
+                Description = addLectureDto.Description,
+                Name = addLectureDto.Name,
+                ParticipantNumber = addLectureDto.ParticipantNumber,
+                StartDate = addLectureDto.StartDate,
+                EndDate = addLectureDto.EndDate
+            };
+
+            return lectureDto;
+        }
+    }
+}
