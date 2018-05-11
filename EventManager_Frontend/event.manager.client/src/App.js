2ec6c1a31c5b51fb5f58aaaea16fa018d409301e
Jakub Woszczyna <kubwosz@gmail.com>
2018-05-11 11:26:29 +0200

    Working and tested adding lecture on frontend

--- EventManager_Frontend/event.manager.client/src/App.js
+++ EventManager_Frontend/event.manager.client/src/App.js
@@ -5,6 +5,7 @@ import HomeNavbar from './components/Navbar';
 import Event from './components/Event';
 import {Label, FormControl, Button, ButtonToolbar} from 'react-bootstrap';
 import NewEvent from './components/NewEvent';
+import NewLecture from './components/NewLecture';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -13,7 +14,8 @@ render() {
       <div className="App">
         <BrowserRouter>
           <div>
-              <Route path='/new' component={NewEvent}/>
+              <Route path='/newEvent' component={NewEvent}/>
+              <Route path='/newLecture' component={NewLecture}/>
               <Route exact path='/' component={Event}/>
           </div>
         </BrowserRouter>
@@ -22,49 +24,4 @@ render() {
 }
 }
 
-
-
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <HomeNavbar/>
-       <h1>
-     <Label>Obsługa konferencji</Label>
-  </h1>
-  <FormControl
-            type="text"
-            placeholder="Enter text"
-            onChange={this.handleChange}
-          />
-          
-<div style={{display: 'flex', justifyContent: 'center'}}>
-    <ButtonToolbar >
-  <Button bsSize="large" bsStyle="primary">GET</Button>
-
-  <Button bsSize="large" bsStyle="success">POST</Button>
-
-  <Button bsSize="large" bsStyle="warning">PUT</Button>
-
-  <Button bsSize="large" bsStyle="danger">DELETE</Button>
-  </ButtonToolbar>
-</div>
-
-     <h1>
-     <Label>Dodaj konferencje</Label>
-  </h1>
-
-<textarea class="form-control" rows="1">Nazwa konferencji</textarea>
-
-
-
- <ButtonToolbar >
-<Button bsSize="large" bsStyle="primary">Dodaj konferencje</Button>
-</ButtonToolbar>
-      </div>
-    );
-  }
-}
-*/
 export default App;
