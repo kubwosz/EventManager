360aa3a4d6c4e32d59c824b842be85884efe83cd
Jakub Woszczyna <kubwosz@gmail.com>
2018-05-17 13:03:26 +0200

    Refactoirzed, added folders, no api calls inf repositry yet

--- EventManager_Frontend/event.manager.client/src/components/Home/index.js
+++ EventManager_Frontend/event.manager.client/src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {withRouter} from "react-router-dom";
 import {Carousel} from "react-bootstrap";
-import myImage from '../tlo.jpg';
+import myImage from '../data/tlo.jpg';
 
 class Home extends React.Component {
     constructor() {
