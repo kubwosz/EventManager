8829e20f23ab11dbd1ab437200a4e6dd96624ba7
Kinga Marek <marekkinga97@gmail.com>
2018-05-24 17:40:34 +0200

    editEvent bootstrap forms

--- EventManager_Frontend/event.manager.client/src/components/editEvent/index.js
+++ EventManager_Frontend/event.manager.client/src/components/editEvent/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {Form, FormGroup, FormControl, ControlLabel, Col, PageHeader} from 'react-bootstrap';
 
 
 class EditEvent extends React.Component {
@@ -27,6 +28,7 @@ class EditEvent extends React.Component {
     
     onChangeName = (event) =>{
         this.setState({eventName: event.target.value})
+        window.confirm('Name changed');
     }
 
     onChangeParticipantNumber = (event) =>{
@@ -75,20 +77,63 @@ class EditEvent extends React.Component {
 
     render() {
         return(
-            <div>
-                <h1> Edytuj wydarzenie</h1>
-                <div className="container">
-                    <input onChange={this.onChangeName} value={this.state.eventName} placeholder="Nazwa" className="form-control"/>
-                    <input onChange={this.onChangeParticipantNumber} value={this.state.eventParticipantNumber === null ? "" : this.state.eventParticipantNumber } placeholder="Liczba uczestników" className="form-control"/>
-                    <input onChange={this.onChangeStartDate} value={this.state.eventStartDate} placeholder="Data rozpoczęcia" className="form-control"/>
-                    <input onChange={this.onChangeEndDate} value={this.state.eventEndDate} placeholder="Data zakończenia" className="form-control" />
-                    <input onChange={this.onChangeDescription} value={this.state.eventDescription} placeholder="Opis" className="form-control"/>
-                    <button onClick={this.editEvent}>Edytuj</button>
-                </div>
-                <div className="container">
-                    <Link to={"/NewLecture/" + this.state.eventID} style={{color: 'black'}} ><button >Dodaj wykład w ramach tego wydarzenia</button></Link>
-                </div>
-            </div>
+            <Form horizontal>
+
+                <FormGroup>
+                <Col sm={2}> </Col>
+                <Col sm={9}> 
+                <PageHeader > Edytuj wydarzenie</PageHeader> 
+                </Col>
+                </FormGroup>
+
+                <FormGroup>
+                <Col componentClass={ControlLabel} sm={2}> Nazwa </Col>
+                <Col sm={9}>
+                <FormControl onBlur={this.onChangeName} defaultValue={this.state.eventName} placeholder={this.state.eventName}/>
+                </Col>
+
+                <Col componentClass={ControlLabel} sm={2}> Liczba uczestników </Col>
+                <Col sm={9}>
+                <FormControl onBlur={this.onChangeParticipantNumber} 
+                defaultValue={this.state.eventParticipantNumber === null ? "" : this.state.eventParticipantNumber}
+                placeholder={this.state.eventParticipantNumber} />
+                </Col>
+
+                <Col componentClass={ControlLabel} sm={2}> Data rozpoczęcia </Col>
+                <Col sm={9}>
+                <FormControl onBlur={this.onChangeStartDate} 
+                defaultValue={this.state.eventStartDate === null ? "" : this.state.eventStartDate} 
+                placeholder={this.state.eventStartDate} />
+                </Col>
+
+                <Col componentClass={ControlLabel} sm={2}> Data zakończenia </Col>
+                <Col sm={9}>
+                <FormControl onBlur={this.onChangeStartDate} 
+                defaultValue={this.state.eventEndDate === null ? "" : this.state.eventEndDate}
+                placeholder={this.state.eventEndDate} />
+                </Col>
+
+                <Col componentClass={ControlLabel} sm={2}> Opis </Col>
+                <Col sm={9}>
+                <FormControl onBlur={this.onChangeDescription} 
+                defaultValue={this.state.eventDescription === null ? "" : this.onChangeDescription}
+                placeholder={this.state.eventDescription} />
+                </Col>
+                </FormGroup>
+
+
+                <FormGroup>
+                    <Col sm={2}></Col>                    
+                    <Col sm={4}>
+                    <button onClick={this.editEvent} className="btn btn-primary">Edytuj</button>
+                    <Link to={"/NewLecture/" + this.state.eventID} style={{color: 'black'}}>
+                    <button className="btn btn-primary"> Dodaj wykład do wydarzenia </button>
+                    </Link>
+                    </Col>
+                    <Col sm={1}></Col>                    
+                </FormGroup>
+
+            </Form>
         );
     }
 }
