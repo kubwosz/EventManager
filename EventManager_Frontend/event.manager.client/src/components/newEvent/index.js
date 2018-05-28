1790c16eb7221423f87b9becd173524c1d9c47d4
Jakub Woszczyna <kubwosz@gmail.com>
2018-05-28 11:09:32 +0200

    changed dateRangePicker texts

--- EventManager_Frontend/event.manager.client/src/components/newEvent/index.js
+++ EventManager_Frontend/event.manager.client/src/components/newEvent/index.js
@@ -112,12 +112,12 @@ class NewEvent extends React.Component {
 
                         <Col componentClass={ControlLabel} sm={2}> Czas rozpoczęcia  </Col>
                         <Col sm={9}>
-                                <TimePicker onChange={this.onChangeStartTime} defaultValue={moment('08:00', "HH:mm")} format={"HH:mm"} minuteStep={5} />
+                                <TimePicker onChange={this.onChangeStartTime} defaultValue={moment('00:00', "HH:mm")} format={"HH:mm"} minuteStep={5} />
                         </Col>
 
                         <Col componentClass={ControlLabel} sm={2}> Czas zakończenia </Col>
                         <Col sm={9}>
-                                <TimePicker onChange={this.onChangeEndTime} defaultValue={moment('08:00', "HH:mm")} format={"HH:mm"} minuteStep={5} />
+                                <TimePicker onChange={this.onChangeEndTime} defaultValue={moment('00:00', "HH:mm")} format={"HH:mm"} minuteStep={5} />
                         </Col>
 
                         <Col componentClass={ControlLabel} sm={2}> Data rozpoczęcia oraz zakończenia </Col>
@@ -125,6 +125,8 @@ class NewEvent extends React.Component {
                             <DateRangePicker
                                 startDate={this.state.startDate}
                                 endDate={this.state.endDate}
+                                endDatePlaceholderText={"Start"}
+                                startDatePlaceholderText={"Koniec"}
                                 onDatesChange={({ startDate, endDate }) => this.setState({startDate, endDate})}
                                 focusedInput={this.state.focusedInput}
                                 onFocusChange={focusedInput => this.setState({ focusedInput })}
